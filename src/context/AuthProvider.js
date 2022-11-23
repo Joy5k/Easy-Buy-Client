12fa561b5d3946acda570734aaaadb621db357b3
Joy <mmehedihasanjoyv@gmail.com>
2022-11-24 00:31:45 +0600

    basice set up

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { createContext } from 'react';
+import app from '../firebase/firebase.config';
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged,
+    updateProfile
+} from 'firebase/auth'
+import { useState } from 'react';
 
-const AuthProvider = () => {
+export const AuthContext = createContext()
+const auth=getAuth(app)
+const AuthProvider = ({ children }) => {
+    const googleProvider = new GoogleAuthProvider();
+    const [loading, setLoading] = useState(true);
+    const [user,setUser]=useState(null)
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const signInWithGoogle = () => {
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    const updateUser = (userInfo) => {
+        return updateProfile(auth.currentUser,userInfo)
+    }
+
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log('user', currentUser);
+            setLoading(false)
+        })
+        return () => unsubscribe();
+},[])
+    const authInfo = {
+        createUser,
+        signIn,
+        signInWithGoogle,
+        updateUser,
+        user,
+        loading,
+        logOut
+    }
     return (
-        <div>
-            
-        </div>
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
     );
-};
+}
 
 export default AuthProvider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  daisyui:{
+    themes:[
+      {
+    easytheme:{
+          primary: '#4361ee',         
+          secondary: "#3a86ff",               
+         accent: "#384971",         
+         neutral: "#405173",
+         "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
