44069645d08520695981810e02cb67315137b9f9
Joy <mmehedihasanjoyv@gmail.com>
2022-11-24 02:11:28 +0600

    impletent  logout button

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -16,14 +16,14 @@ const Navbar = () => {
     
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-accent text-white rounded-sm">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><Link>Item 1</Link></li>
+              <li><Link>Home</Link></li>
               <li tabIndex={0}>
                 <Link className="justify-between">
                   Parent
@@ -41,10 +41,10 @@ const Navbar = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
-            <li><Link>Item 1</Link></li>
+            <li><Link>Home</Link></li>
             <li tabIndex={0}>
               <Link>
-                Parent
+                Category
                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </Link>
               <ul className="p-2">
