f9b5100b6b894f053f24fa42173c04249d226eac
Joy <mmehedihasanjoyv@gmail.com>
2022-11-28 20:20:08 +0600

    create jwt token when use sign up

--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -22,18 +22,7 @@ const SignUp = () => {
     const name = form.name.value;
     const userCategory = form.userOrSeller.value;
 
-    fetch('https://y-dun-gamma.vercel.app/jwt', {
-      method: 'POST',
-      headers: {
-        'content-type':'application/json'
-      },
-      body:JSON.stringify(email)
-    })
-      .then(res => res.json())
-      .then(data => {
-        localStorage.setItem('accessToken',data.token)
-        console.log(data)
-      })
+   
 
     const userInfo = {
       email,
@@ -49,6 +38,23 @@ const SignUp = () => {
       .then((result) => {
         const user = result.user;
         console.log(user);
+        const currentUser = {
+          email:user.email
+        }
+        fetch('https://y-dun-gamma.vercel.app/jwt', {
+          method: 'POST',
+          headers: {
+            'content-type':'application/json'
+          },
+          body:JSON.stringify(currentUser)
+        })
+          .then(res => res.json())
+          .then(data => {
+            localStorage.setItem('accessToken',data.token)
+            console.log(data)
+          })
+
+
         UpdateUserInfo(data)
           .then(() => {
             fetch("https://y-dun-gamma.vercel.app/user", {
